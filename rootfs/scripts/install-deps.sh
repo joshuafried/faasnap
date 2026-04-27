db1f6f2bdb94bd56085e735d54ee208adbe3ae7d
Ben Holmes <bcwh@csail.mit.edu>
2026-04-27 01:05:23 +0000

    install python deps in venv

--- rootfs/scripts/install-deps.sh
+++ rootfs/scripts/install-deps.sh
@@ -4,7 +4,7 @@ set -ex
 
 echo deb http://archive.ubuntu.com/ubuntu noble universe >> /etc/apt/sources.list
 apt update
-apt install -y nodejs libgl1 gfortran ruby php-cli python3-pil libopenblas-dev npm
+apt install -y nodejs libgl1 gfortran ruby php-cli python3-pil libopenblas-dev npm python3.12-venv
 
 apt update
 apt install -f -y openssh-server build-essential gpg wget libblas3 liblapack3 liblapack-dev libblas-dev gfortran libffi-dev python3.12 python3-pip openjdk-21-jre-headless libcjson-dev
@@ -15,4 +15,6 @@ tar xzvf cmake-3.22.2-linux-x86_64.tar.gz
 export PATH=$PATH:/opt/cmake-3.22.2-linux-x86_64/bin/
 popd
 
-MAKEFLAGS="-j54" pip3 install --break-system-packages wheel six scikit-learn flask pillow pyaes chameleon pandas tensorflow torch torchvision minio psutil keras-preprocessing keras-applications opencv-python rdtsc
+python3 -m venv /app/python/
+
+MAKEFLAGS="-j54" /app/python/bin/pip3 install --break-system-packages wheel six scikit-learn flask pillow pyaes chameleon pandas tensorflow torch torchvision minio psutil keras-preprocessing keras-applications opencv-python rdtsc
